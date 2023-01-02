a9e528a89d13d88ae1244aa4a03b403e0fb5a526
nunswn <nunswn@gmail.com>
2023-01-02 16:23:14 +0700

    push

---
+++ Basic JS/ข้อที่ 1/Homework1.js
@@ -0,0 +1,20 @@
+5 + "34"            // 
+5 ** 4              // 
+5 - "4"             // 
+10 % 5
+5 % 10;
+(2 ** 5) % 2
+"Java" + "Script"
+" " + " "
+" " + 0
+20 + 4 + "15"
+true + true
+true + false
+false + true
+false - true
+false - true + false - true + false;
+(true + true) ** 4
+19 + true + "20"
+3 - 4
+"Bob" - "bill"
+"Code" + "Camp" + true + false + null + undefined
---
+++ Basic JS/ข้อที่ 3/Readme.md
@@ -0,0 +1,3 @@
+### CodeCamp #13
+    1.ณัฐนันท์ สุวรรณนึก
+    2.สร้าง String ที่มีค่า "Hello, It's me. "Mario"."
